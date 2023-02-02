39493ac050ec6c9ceab9365cba5ee1a4a8f3ce19
U-BEN-LAPTOP\Ben10 <bmfioresi@crimson.ua.edu>
2023-02-01 20:04:43 -0600

    added delete and find functions

--- binomial-heap/binomial-heap.js
+++ binomial-heap/binomial-heap.js
@@ -95,8 +95,8 @@ function linkTrees (tree, other) {
  * @return {int} -1 (a < b), 0 (a == b), 1 (a > b)
  */
 BinomialHeap.prototype.compare = function (a, b) {
-    if (a.key > b.key) return 1;
-    if (a.key < b.key) return -1;
+    if (parseFloat(a.key) > parseFloat(b.key)) return 1;
+    if (parseFloat(a.key) < parseFloat(b.key)) return -1;
     return 0;
 }
 
@@ -150,7 +150,6 @@ BinomialHeap.prototype.insert = function (key) {
     temp.head = newnode;
     temp.nodeCount++;
     this.union(temp);
-    if (this.head === undefined) console.log("problem\n");
     return newnode;
 }
 
@@ -181,7 +180,7 @@ BinomialHeap.prototype.findMin = function () {
 function removeTreeRoot (heap, root, prev) {
     // remove root
     if (root === heap.head) heap.head = root.sibling;
-    else prev.sibling = root.sibling;
+    else if (prev) prev.sibling = root.sibling;
 
     // reverse order of root's children and make new heap
     var newhead;
@@ -218,12 +217,66 @@ BinomialHeap.prototype.extractMin = function () {
         nextprev = next;
         next = next.sibling;
     }
-    removeTreeRoot(this, min, minPrev);
+    removeTreeRoot(this, min, minprev);
     this.nodeCount--;
 
     return min;
 }
 
+/**
+ * decreases the key of the current node
+ * @param {_Node} node 
+ * @param {int} key 
+ */
+BinomialHeap.prototype.decreaseKey = function (node, key) {
+    node.key = key;
+    var cur = node;
+    var par = cur.parent;
+    
+    while (par && this.compare(cur, par) < 0) {
+        var temp = cur.key;
+        cur.key = par.key;
+        par.key = temp;
+        cur = par;
+        par = par.parent;
+    }
+}
+
+/**
+ * @param {_Node} head 
+ * @param {int} key 
+ * @return {_Node} the found node if found, otherwise undefined
+ */
+function find (head, key) {
+    while (head) {
+        if (head.key == key) return head;
+        var found = find(head.child, key);
+        if (found) return found;
+        head = head.sibling;
+    }
+    return undefined;
+}
+
+/**
+ * searches for key and deletes it if found
+ * @param {int} key 
+ * @return {boolean} whether the key is actually deleted
+ */
+BinomialHeap.prototype.delete = function (key) {
+    var found = find(this.head, key);
+    if (!found) return false;
+
+    this.decreaseKey(found, Number.NEGATIVE_INFINITY); // makes node have unique minimum key of -inf
+    this.extractMin(); // remove it
+
+    this.nodeCount--;
+    return true;
+}
+
+/**
+ * prints the values of the keys of a tree
+ * @param {_Node} head 
+ */
 function printTree (head) {
     while (head) {
         document.body.append(head.key + " ");
@@ -232,6 +285,10 @@ function printTree (head) {
     }
 }
 
+/**
+ * prints the trees of a heap, with a '|' to separate them
+ * @param {_Node} head 
+ */
 function printHeap (head) {
     while (head) {
         document.body.append(head.key + " ");
@@ -241,11 +298,71 @@ function printHeap (head) {
     }
 }
 
-var heap = new BinomialHeap();
+/**
+ * print statement format details:
+ * 
+ * trees can be size 1, 2, 4, 8, etc.
+ * trees of size 4+ are grouped into sets of 4
+ * those sets look like this:
+ *                   1
+ *                 2 4
+ *                 3
+ * they will show up in the output as:
+ *             1 2 3 4 |
+ * so, trees of size 8 would be two sets of 4:
+ *                     1
+ *                 2 6 8
+ *               3 5 7
+ *               4
+ * and output as 
+ *            1 2 3 4 5 6 7 8 |
+ * so, a group of seven total items would look like this:
+ *              1  2    4
+ *                 3  5 7
+ *                    6
+ * and ouput as
+ *           1 | 2 3 | 4 5 6 7 |
+ */
 
+var heap = new BinomialHeap();
+// random numbers to use for testing
+// DELETE LATER!
+heap.insert(1);
+heap.insert(48);
+heap.insert(5);
+heap.insert(34);
+heap.insert(20);
+heap.insert(56);
+heap.insert(17);
+
+/**
+ * inserts a key from user input
+ * @param {int} key 
+ */
 function insertKey(key) {
     document.body.append(document.createElement("br"));
     heap.insert(key);
     printHeap(heap.head);
 }
 
+/**
+ * searches for and deletes a key from user input
+ * @param {int} key 
+ */
+function deleteKey(key) {
+    document.body.append(document.createElement("br"));
+    var found = heap.delete(key);
+    if (found) printHeap(heap.head);
+    else document.body.append("key not found");
+}
+
+/**
+ * searches for a key from user input
+ * @param {int} key 
+ */
+function findKey(key) {
+    document.body.append(document.createElement("br"));
+    var found = find(heap.head, key);
+    if (found) document.body.append(true);
+    else document.body.append(false);
+}
